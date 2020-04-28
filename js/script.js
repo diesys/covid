5235307cc0fb13fdb34bd9603d66a826b00b22be
sbiego <forsesim@anche.no>
2020-04-29 00:50:19 +0200

    too much done, almost finished

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
 // changes colors to link and titles and text element with THEME-COLORED class
 function changeColor(hue) {
+    anim_duration = .5
+    if(!hue) {
+        paletteHue = [0, 15, 25, 40, 50, 75, 100, 125, 145, 185, 205, 240]
+        hue = paletteHue[Math.floor(Math.random() * 12)]
+    }
     base_clr = "hsla(" + hue + ",60%,40%,1)"
     base_alpha_clr = "hsla(" + hue + ",60%,40%,.8)"
     dark_clr = "hsla(" + hue + ",60%,20%,1)"
     semilight_clr = "hsla(" + hue + ",70%,60%,1)"
     light_clr = "hsla(" + hue + ",80%,80%,1)"
     // ultralight_clr = "hsla(" + hue + ",100%,90%,1)"
-    anim_duration = .5
     document.querySelectorAll('.theme-colored').forEach(element => {
         if(element.classList.contains('darktext'))
             gsap.to(element, anim_duration, {css: {color: dark_clr}})
@@ -14,13 +18,14 @@ function changeColor(hue) {
             gsap.to(element, anim_duration, {css: {color: light_clr}})
         if(element.classList.contains('text'))
             gsap.to(element, anim_duration, {css: {color: base_clr}})
-            // gsap.to(element, anim_duration, {css: {color: base_clr, background: light_clr}})
         if(element.classList.contains('background')) 
             gsap.to(element, anim_duration, {css: {background: base_clr, color: light_clr}})
         if(element.classList.contains('backgroundAlpha')) 
             gsap.to(element, anim_duration, {css: {background: base_alpha_clr, color: light_clr}})
         if(element.classList.contains('browser'))
             gsap.to(element, anim_duration, {attr: {content: base_clr}})
+        if(element.classList.contains('image'))
+            gsap.to(element, anim_duration, {css: {filter: 'hue-rotate('+hue+'deg)'}})
     })
 
     // gsap.to(document.querySelector('header'), anim_duration, {css: {background: 'linear-gradient('+ dark_clr + ',' + base_alpha_clr}})
@@ -31,9 +36,6 @@ function toggleLang() {
     document.querySelector('body').classList.toggle('it')
 }
 
-paletteHue = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34]
-
 window.addEventListener('load', function() {
-    // changeColor(Math.floor(Math.random()*36)+'0')
-    changeColor(paletteHue[Math.floor(Math.random() * 18)] + '0')
+    changeColor()
 })
