d119077d0b3352926516450892eff2983a633b72
sbiego <forsesim@anche.no>
2020-04-29 05:38:00 +0200

    smooth scrolling and menu selected areas

--- js/script.js
+++ js/script.js
@@ -36,6 +36,29 @@ function toggleLang() {
     document.querySelector('body').classList.toggle('it')
 }
 
+
+let mainNavLinks = document.querySelectorAll("#menu li a");
+let mainSections = document.querySelectorAll("#container .list");
+
+let lastId;
+let cur = [];
+
+// https://css-tricks.com/sticky-smooth-active-nav/
+window.addEventListener("scroll", event => {
+    let fromTop = window.scrollY;
+
+    mainNavLinks.forEach(link => {
+        let section = document.querySelector(link.hash);
+
+        if (section.offsetTop <= fromTop && 
+            section.offsetTop + section.offsetHeight > fromTop) {
+            link.classList.add("active");
+        } else {
+            link.classList.remove("active");
+        }
+    });
+});
+
 window.addEventListener('load', function() {
     changeColor()
 })
